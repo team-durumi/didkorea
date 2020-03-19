ed75b26978af555b48c5550a4a9ee5fc1c5385db
Yongsun Lee <yslee264@gmail.com>
2020-03-19 18:26:23 +0900

    basic implementation for products tabs

--- src/pages/products.js
+++ src/pages/products.js
@@ -5,15 +5,15 @@ import get from 'lodash/get'
 import Post from 'templates/post'
 import Meta from 'components/meta'
 import Layout from 'components/layout'
+import Tab from 'components/tab'
 import { products, series } from '../../data/product-terms.yml'
-console.log(products, series)
 
-const ProductsIndex = ({ data, location, products, series }) => {
+const ProductsIndex = ({ data, location }) => {
   return (
     <Layout location={location}>
       <Meta site={get(data, 'site.meta')} />
       <div className="container">
-        <Tab />
+        <Tab series={series} />
         <br />
       </div>
     </Layout>
