461e3fd2e6d9c32f223488d95fee5c2bf5a1b49b
Yongsun Lee <yslee264@gmail.com>
2020-03-20 16:57:00 +0900

    header footer components

--- src/components/navi/index.js
+++ src/components/navi/index.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+const navStyle = {
+  height: 100,
+  backgroundColor: '#020980',
+}
+
+const navLinkStyle = {
+  color: 'white',
+}
+
 function Navi(props) {
   const { location, title, items } = props
   return (
     <nav
       className="navbar navbar-expand flex-column flex-md-row border-bottom box-shadow"
-      style={{ height: 100 + 'px' }}
+      style={navStyle}
     >
       <div className="container">
         <Link className="text-center mr-md-auto" to="/">
-          <h1 className="navbar-brand mb-0">{title}</h1>
+          <h1 className="navbar-brand mb-0" style={navLinkStyle}>
+            {title}
+          </h1>
         </Link>
         <div className="navbar-nav-scroll">
           <ul className="navbar-nav bd-navbar-nav flex-row">
@@ -24,7 +35,11 @@ function Navi(props) {
                       : 'nav-item'
                   }
                 >
-                  <Link to={item.link} className="nav-link">
+                  <Link
+                    to={item.link}
+                    className="nav-link"
+                    style={navLinkStyle}
+                  >
                     {item.title}
                   </Link>
                 </li>
