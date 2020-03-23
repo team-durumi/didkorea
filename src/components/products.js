81bddf7a09c53a3aba458f7dd1f9cbfd0740c942
Yongsun Lee <yslee264@gmail.com>
2020-03-24 01:01:26 +0900

    제품소개페이지 표에서 col 로 변경

--- src/components/products.js
+++ src/components/products.js
@@ -4,89 +4,75 @@ const productPageStyle = {
   padding: 50,
 }
 
-const tableHeadStyle = {
-  backgroundColor: 'lightblue',
+const cardHeadStyle = {
+  backgroundColor: '#CACBCA',
   color: 'white',
+  textAlign: 'center',
+  // height: '5rem'
 }
 
 const tableNameStyle = {
-  backgroundColor: 'lightgrey',
+  backgroundColor: '#CACBCA',
+}
+
+const featuresLiStyle = {
+  paddingBottom: 10,
 }
 
 export default function Products({ products }) {
   console.log(products)
   return (
     <div style={productPageStyle}>
-      <h1>title</h1>
       <h2>subtitle goes here</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th style={tableHeadStyle} rowSpan="2">
-              Name
-            </th>
-            {products.map((product, key) => {
-              return (
-                <td key={key} style={tableNameStyle}>
-                  <h5 style={{ textAlign: 'center' }}>{product.name}</h5>
-                </td>
-              )
-            })}
-          </tr>
-          <tr>
-            {/*<th style={tableHeadStyle}></th>*/}
-            {products.map((product, key) => {
-              return (
-                <td key={key}>
-                  <img src={product.image} width="100%" />
-                </td>
-              )
-            })}
-          </tr>
-          <tr>
-            <th style={tableHeadStyle}>Features</th>
-            {products.map((product, key) => {
-              return (
-                <td key={key}>
-                  <p>{product.features}</p>
-                </td>
-              )
-            })}
-          </tr>
-          <tr>
-            <th style={tableHeadStyle}>Functions</th>
-            {products.map((product, key) => {
-              return (
-                <td key={key}>
-                  <img src={product.functions} width="50%" />
-                </td>
-              )
-            })}
-          </tr>
-          <tr>
-            <th style={tableHeadStyle}>Main Uses</th>
-            {products.map((product, key) => {
-              return (
-                <td key={key}>
-                  <span>{product['main-uses']} </span>
-                </td>
-              )
-            })}
-          </tr>
-          <tr>
-            <th style={tableHeadStyle}>PDF</th>
-            {products.map((product, key) => {
-              return (
-                <td key={key}>
-                  <a href={product.files} target="_blank">
-                    {product.name}
-                  </a>
-                </td>
-              )
-            })}
-          </tr>
-        </tbody>
-      </table>
+      <div className="row">
+        {products.map((product, key) => {
+          return (
+            <div className="col-md-4 my-3">
+              <div className="card">
+                <h5 className="card-header" style={cardHeadStyle}>
+                  {product.name}
+                </h5>
+                <div style={{}}>
+                  <img
+                    className="card-img-top"
+                    src={product.image}
+                    style={{ width: '100%' }}
+                  />
+                </div>
+                <div className="card-body" style={{}}>
+                  <div style={{}}>
+                    <ul style={{ paddingLeft: 0 }}>
+                      {product.features.map((item, key) => {
+                        return (
+                          <li className="list-unstyled" style={featuresLiStyle}>
+                            {item}
+                          </li>
+                        )
+                      })}
+                    </ul>
+                  </div>
+                  <div>
+                    <img src={product.functions} />
+                  </div>
+                  <div>
+                    <span>{product['main-uses']} </span>
+                  </div>
+                  <div>
+                    <a
+                      href={product.files}
+                      target="_blank"
+                      style={{ fontWeight: 'bold' }}
+                    >
+                      <i className="fa fa-download mr-2" />
+                      <span>{product.name}</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
