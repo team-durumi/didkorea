c6901f2b5f4b4843ebfaa921d5fc2372978fba5e
Sun <suncrud@Suns-iMac.local>
2020-03-28 20:11:29 +0900

    모바일 메뉴

--- src/components/navi/index.js
+++ src/components/navi/index.js
@@ -12,45 +12,62 @@ const navLinkStyle = {
 function Navi(props) {
   const { location, title, items } = props
   return (
-    <nav
-      className="navbar navbar-expand flex-column flex-md-row border-bottom box-shadow py-lg-4"
-      style={navStyle}
-    >
-      <div className="container">
-        <Link className="text-center mr-md-auto" to="/">
-          <img
-            className="navbar-brand mb-0"
-            style={{ color: 'white', width: '50%', left: 0, display: 'block' }}
-            src="/did-logo.png"
-            alt={title}
-          />
-        </Link>
-        <div className="navbar-nav-scroll">
-          <ul className="navbar-nav bd-navbar-nav flex-row">
-            {items.map((item, key) => {
-              return (
-                <li
-                  key={key}
-                  className={
-                    location.pathname === item.link
-                      ? 'nav-item active'
-                      : 'nav-item'
-                  }
-                >
-                  <Link
-                    to={item.link}
-                    className="nav-link"
-                    style={navLinkStyle}
+    <div style={navStyle}>
+      <div className="container px-0">
+        <nav className="navbar navbar-expand-lg px-0">
+          <Link className="navbar-brand " to="/">
+            <img
+              className="mb-0 text-center mr-md-auto"
+              style={{
+                color: 'white',
+                width: 150,
+                left: 0,
+                display: 'inline-block',
+              }}
+              src="/did-logo.png"
+              alt={title}
+            />
+          </Link>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i className="fa fa-bars" style={{ color: 'white' }} />
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+              {items.map((item, key) => {
+                return (
+                  <li
+                    key={key}
+                    className={
+                      location.pathname === item.link
+                        ? 'nav-item active'
+                        : 'nav-item'
+                    }
                   >
-                    {item.title}
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
+                    <Link
+                      to={item.link}
+                      className="nav-link"
+                      style={navLinkStyle}
+                    >
+                      {item.title}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        </nav>
       </div>
-    </nav>
+    </div>
   )
 }
 
