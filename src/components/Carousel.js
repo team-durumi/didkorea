b6f557ff50afe0651a96f2d77707032dd7cf813a
Sun <suncrud@Suns-iMac.local>
2020-03-30 14:03:07 +0900

    메인페이지 carousel control 버튼 삭제

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -33,30 +33,6 @@ export default function Carousel({ slides }) {
             )
           })}
         </div>
-        <a
-          className="carousel-control-prev"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a
-          className="carousel-control-next"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="sr-only">Next</span>
-        </a>
       </div>
     </div>
   )
